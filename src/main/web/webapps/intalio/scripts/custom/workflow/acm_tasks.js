abdfd81e5b50f518c130eed0e86111d444a21484
Ihab EL ALAMI <ihabo01@gmail.com>
2015-02-09 23:10:08 +0100

    fixing task list

--- src/main/web/webapps/intalio/scripts/custom/workflow/acm_tasks.js
+++ src/main/web/webapps/intalio/scripts/custom/workflow/acm_tasks.js
@@ -9,8 +9,10 @@ var relatedTo={
         "sClass": "center qsdqsd",
         "sWidth": width * 0.07
     };
+if(taskOptions.aoColumns.length<=11){
 taskOptions.aoColumns.splice(taskOptions.aoColumns.length-2,0,relatedTo);
 taskOptions.aoColumns.splice(taskOptions.aoColumns.length-2,0,state);
+}
 });
 
 function addACMContent(obj,items){
@@ -20,4 +22,4 @@ function addACMContent(obj,items){
 			items[items.length]="<a href='/intalio/index.htm#/cases//"+caseType+"/"+caseId+"' onclick=\"javascript:selectMenuAndChangepage(this,\'cases\',\'cases.htm\');\">Case</a>";			
 			items[items.length]="<i class='green ace-icon fa fa-info bigger-120' onmouseover='updateStatusTab(\""+caseType+"\",\""+caseId+"\",\".information\");showInformation();'></i>";
 			
-			}
+}
