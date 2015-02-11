1d5b214f1b8577cb7d43c590d6cac3dbc0763d60
Ihab EL ALAMI <ihabo01@gmail.com>
2015-02-11 17:22:41 +0100

    H2 init SQL file

--- src/main/resources/databases/ACM-Init-H2.sql
+++ src/main/resources/databases/ACM-Init-H2.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS bpms.tempo_casetype;
 CREATE TABLE bpms.tempo_casetype (
 		id VARCHAR(250),
 		description VARCHAR(250),
-		status_diagram VARCHAR(250)
+		status_diagram VARCHAR(250),
+		case_form VARCHAR(250)
 	);
 
 insert into module_action values(73,'Cases',NULL,0,NULL,1);
